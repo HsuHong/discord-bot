6622f8230645639850aeaa1233c50e4c2a682448
Matthieu <89matt89.md@gmail.com>
2020-11-04 09:17:13 +0100

    Create ecosystem file for pm2 module

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps : [{
+      name: 'arendelleodyssey-bot',
+      script: 'src/index.js',
+  
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      autorestart: true,
+      watch: true,
+      ignore_watch : ["node_modules", "data", "logs", ".git"],
+      max_memory_restart: '500M'
+    }]
+  };
