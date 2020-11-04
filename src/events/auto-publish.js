4e8a460416eb2571af5d4ad7eb612eb6638dcf4f
Matthieu <89matt89.md@gmail.com>
2020-11-04 18:37:43 +0100

    fix error

--- src/events/auto-publish.js
+++ src/events/auto-publish.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const fetch = require('node-fetch');
 
-module.exports = function(client, message, config){
+module.exports = async function(client, message, config){
     const { options: { http }, rest } = client;
     if (message.channel.type === 'news') {
 		await fetch(`${http.api}/v${http.version}/channels/${message.channel.id}/messages/${message.id}/crosspost`,
