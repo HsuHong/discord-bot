8607077ba12bada54e1b06dc92df27e6f00f968d
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2020-11-08 00:05:42 +0100

    Fix publish for djs 12

--- src/events/auto-publish.js
+++ src/events/auto-publish.js
@@ -4,11 +4,11 @@ const fetch = require('node-fetch');
 module.exports = async function(client, message, config){
     const { options: { http }, rest } = client;
     if (message.channel.type === 'news') {
-		await fetch(`${http.host}/api/v${http.version}/channels/${message.channel.id}/messages/${message.id}/crosspost`,
+		await fetch(`${http.api}/v${http.version}/channels/${message.channel.id}/messages/${message.id}/crosspost`,
 			{
 				method: 'POST',
 				headers: {
-					'Authorization': `Bot ${config.discord.token}`,
+					'Authorization': `${rest.tokenPrefix} ${config.discord.token}`,
 				},
 			},
 		)
