26520f125a09e1cf1092f15caea26fc0ed470bbf
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2020-11-08 00:11:00 +0100

    Update auto-publish.js

--- src/events/auto-publish.js
+++ src/events/auto-publish.js
@@ -4,7 +4,9 @@ const fetch = require('node-fetch');
 module.exports = async function(client, message, config){
     const { options: { http }, rest } = client;
     if (message.channel.type === 'news') {
-		await fetch(`${http.api}/v${http.version}/channels/${message.channel.id}/messages/${message.id}/crosspost`,
+		const url = `${http.api}/v${http.version}/channels/${message.channel.id}/messages/${message.id}/crosspost`
+		console.log(url)
+		await fetch(url,
 			{
 				method: 'POST',
 				headers: {
