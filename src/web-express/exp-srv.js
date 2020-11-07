22c5e83ef1a77741a933208c260b1b9fe2fee155
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2020-11-07 21:57:08 +0100

    Update exp-srv.js

--- src/web-express/exp-srv.js
+++ src/web-express/exp-srv.js
@@ -5,14 +5,13 @@ module.exports = function(client, config){
     const port = 8080 // Port 8080 will be proxied by apache2's module "proxy_http" to its domain
 
     app.get('/', (req, res)=>{
-        res.send(`
-            ${client.user.username}\'s dashboard coming later...
-            You can use ${config.discord.prefix}help in the Discord server for more commands!
-
-            --- STATUS ---
-            ${client.user.tag} is online!
-            My user ID is ${client.user.id}
-        `)
+        res.send(`${client.user.username}\'s dashboard coming later...`)
+        res.send(`You can use ${config.discord.prefix}help in the Discord server for more commands!`)
+        res.send('')
+        res.send('')
+        res.send('--- STATUS ---')
+        res.send(`${client.user.tag} is online!`)
+        res.send(`My user ID is ${client.user.id}`)
     })
     
     app.listen(port, () =>{
