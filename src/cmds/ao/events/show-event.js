c4ba80b2c018d93e5541fd4afa1b4862f46306ce
Matthieu <89matt89.md@gmail.com>
2020-11-26 18:30:29 +0100

    Update show-event.js

--- src/cmds/ao/events/show-event.js
+++ src/cmds/ao/events/show-event.js
@@ -1,9 +1,16 @@
+const Discord = require('discord.js')
 module.exports = function(message, args, command, client, prefix, config, sql){
     sql.query("SELECT * FROM events ORDER BY date_start DESC LIMIT 1", (err, res) =>{
         if (err) {
             console.error(err)
             message.channel.send('Error :/\n' + err)
         }
-        console.log(res)
+        let embed = new Discord.MessageEmbed()
+        embed.setTitle(res[0].name)
+        if (res[0].description != null) embed.setDescription(res[0].description)
+        embed.addField('Start date', new Date(res[0].date_start).parse())
+        if (res[0].date_end != null) embed.addField('End date', new Date(res[0].date_end).parse())
+        embed.setFooter('ID: ' + res[0].id)
+        message.channel.send(embed)
     })
 }
