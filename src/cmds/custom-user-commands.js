63fdf2d46997f02d1cf62d6b0c8a02b898848d5b
Matthieu <89matt89.md@gmail.com>
2020-12-07 18:32:27 +0100

    Create custom-user-commands.js

---
+++ src/cmds/custom-user-commands.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js')
+
+function randomItem(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+module.exports = function(message, client, config, sql){
+    sql.query("SELECT DISTINCT `command-name` FROM `mention_responses`", (err, result)=>{
+        if (err) return
+
+        result.forEach(r=>{
+            var command = r['command-name']
+            if (message.content.toLowerCase() == prefix + command.toLowerCase()){
+                sql.query("SELECT `message` FROM `mention_responses` WHERE `command-name` = ?", command, (err, result)=>{
+                    if (err){
+                        console.error(err)
+                        client.users.cache.find(u => u.id == config.discord.owner_id).send(`:warning: Error fetching custom mention msg: \`\`\`${err}\`\`\``)
+                    } else {
+                        var messages = []
+                        result.forEach(r=>messages.push(r.message))
+                        if (messages.length < 1) return
+                        message.channel.send(randomItem(messages))
+                    }
+                })
+            }
+        })
+    })
+}
