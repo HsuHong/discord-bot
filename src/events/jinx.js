6bdc4564cba3778f95e01944822bbdc95efd4951
Matthieu <89matt89.md@gmail.com>
2020-12-08 18:19:40 +0100

    Update jinx.js

--- src/events/jinx.js
+++ src/events/jinx.js
@@ -5,6 +5,7 @@ module.exports = function(client, message){
     if (oldmsg == undefined) oldmsg = message
     else {
         if (oldmsg.content.toLowerCase() == message.content.toLowerCase()){
+            if (oldmsg.author.id == message.author.id) return
             message.channel.send('https://cdn.discordapp.com/emojis/755280811447812127.png?v=1')
         }
         oldmsg = message
