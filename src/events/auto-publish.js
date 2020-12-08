c27ec07ca807151d681e96a26a35b0d264370257
Matthieu <89matt89.md@gmail.com>
2020-12-08 15:21:04 +0100

    disable crosspost in announcements channel

--- src/events/auto-publish.js
+++ src/events/auto-publish.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js')
 const fetch = require('node-fetch');
 
 module.exports = async function(client, message, config){
+	if (message.channel.id == '729985657656836096') return // Don't do this in announcements channel (we'll do it manually)
 	var token
 	if (client.user.id == config.discord.bot_id){
         token = config.discord.token
