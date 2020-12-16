5d8e294c8d752e8b6f2076b291a2a5ddc278f989
Matthieu <89matt89.md@gmail.com>
2020-12-16 14:12:32 +0100

    Update custom-commands-list.js

--- src/cmds/user-cmds/custom-commands-list.js
+++ src/cmds/user-cmds/custom-commands-list.js
@@ -8,19 +8,25 @@ module.exports = function(client, message, prefix, config, sql){
                     console.error(err)
                     message.channel.send(':negative_squared_cross_mark: ' + message.author.username + ', an error has been happened. This is reported.')
                     client.users.cache.find(u => u.id == config.discord.owner_id).send(`:warning: Error fetching custom mention list msg: \`\`\`${err}\`\`\``)
-                  } else {
-                    var list = []
-                    result.forEach(r=>{
-                        var command = r['command-name']
-                        var username = client.users.cache.get(String(r.user)).username
-                        if (username == undefined) username = 'Username not found 🤦‍♂️'
-                        list.push(`\`${prefix}${command}\` - ${username}`)
-                    })
-                    let embed = new Discord.MessageEmbed
-                    embed.setTitle('User\'s command list:')
-                    .setColor('#00FF22')
-                    .setDescription(list.join('\n'))
-                    message.channel.send(embed)
+                } else {
+                    try{
+                        var list = []
+                        result.forEach(r=>{
+                            var command = r['command-name']
+                            var username = client.users.cache.get(String(r.user)).username
+                            if (username == undefined) username = 'Username not found 🤦‍♂️'
+                            list.push(`\`${prefix}${command}\` - ${username}`)
+                        })
+                        let embed = new Discord.MessageEmbed
+                        embed.setTitle('User\'s command list:')
+                        .setColor('#00FF22')
+                        .setDescription(list.join('\n'))
+                        message.channel.send(embed)
+                    } catch (err) {
+                        console.error(err)
+                        message.channel.send('An error has been happened. This is reported.')
+                        client.users.cache.find(u => u.id == config.discord.owner_id).send(`:warning: Error fetching custom mention msg list: \`\`\`${err}\`\`\``)
+                    }
                 }
             })
         } catch (err) {
