fc33d17f6bd22acd14753e28590df2e53e62234d
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2020-12-22 20:16:33 +0100

    Fix msg bug

--- src/events/jinx.js
+++ src/events/jinx.js
@@ -4,8 +4,8 @@ var oldmsg = undefined
 module.exports = function(client, message){
     if (oldmsg == undefined) oldmsg = message
     else {
-        if (oldmsg.author.bot || message.author.bot) return
         if (oldmsg.content.toLowerCase() == message.content.toLowerCase()){
+            if (oldmsg.author.bot || message.author.bot) return
             if (oldmsg.author.id == message.author.id) return
             if (oldmsg.channel.id != message.channel.id) return
             message.channel.send('https://cdn.discordapp.com/emojis/755280811447812127.png?v=1')
